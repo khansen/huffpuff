9fd3044a243989b00328ba0e8e1ec12410272c29
Kent Hansen <kentmhan@gmail.com>
2009-08-05 20:13:19 +0200

    update license headers

--- charmap.h
+++ charmap.h
@@ -1,28 +1,19 @@
 /*
- * $Id: charmap.h,v 1.1 2004/06/30 07:56:19 kenth Exp $
- * $Log: charmap.h,v $
- * Revision 1.1  2004/06/30 07:56:19  kenth
- * Initial revision
- *
- */
+    This file is part of huffpuff.
 
-/**
- *    (C) 2004 Kent Hansen
- *
- *    The XORcyst is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
- *
- *    The XORcyst is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU General Public License
- *    along with The XORcyst; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
+    huffpuff is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    huffpuff is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with huffpuff.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef CHARMAP_H
 #define CHARMAP_H
